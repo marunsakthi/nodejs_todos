c8c7ce4f88ffabf26805de25c886ed8638890dab
marunsakthi <58545334+marunsakthi@users.noreply.github.com>
2025-11-17 12:24:48 +0530

    Update todos.router.js
    done job todos end point added router

--- todos.router.js
+++ todos.router.js
@@ -16,6 +16,8 @@ todoRouter.get('/:id',httpGetTodo)
 todoRouter.post('/',httpAddNewTodo)
 todoRouter.put('/:id',httpUpdateTodo)
 todoRouter.delete('/:id',httpDeleteTodo)
+todoRouter.patch('/:id',httpDoneTodo)
 
 
 module.exports = todoRouter;
+
