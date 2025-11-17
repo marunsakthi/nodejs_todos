652258771543d86ed101e67db6bb6ea1a3aaa1e1
marunsakthi <58545334+marunsakthi@users.noreply.github.com>
2025-11-17 12:19:51 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const todoRouter = require("./routes/todos/todos.router");
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.use(express.json());
+
+app.use("/todo", todoRouter);
+
+app.use("/", (req, res) => {
+  return res.status(200).send("Hello Todos");
+});
+
+app.listen(PORT,() => {
+    console.log(`Listening on port ${PORT}...`)
+})
